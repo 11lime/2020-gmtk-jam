a820c8ed1d68b19aa07e9fac71bf71363a0350a6
Largo47 <ryu46@o2.pl>
2020-07-11 10:36:02 +0200

    Controls update

--- Ghost.gd
+++ Ghost.gd
@@ -4,7 +4,6 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var counter : int = 0
 var possessed : int = 1
 export var speed : int 
 export var JumpForce : int 
@@ -33,20 +32,15 @@ func _physics_process(delta):
 	vel.y = gravity
 	if possessed == 1: 
 		print(possessed)
-		print(counter)
 		if Input.is_action_pressed("ui_left"):
 			vel.x -= speed
 		if Input.is_action_pressed("ui_right"):
 			vel.x += speed
 		if Input.is_action_pressed("ui_accept"):  
 			### this is a mess
-			if possessed == 1:
-				possessed = 2  ### mess ends
+			possessed = 2  ### mess ends
 	elif possessed == 2: 
-		 ### also mess
-		if possessed == 2:
+		if Input.is_action_pressed("ui_accept"):  
 			possessed = 1  ### mess ends
 		print(possessed)
-		print(counter)
 	vel = move_and_slide(vel,Vector2.UP)
-	counter += 1
