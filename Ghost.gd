f6fb6a9019cf26fef03a71b10a1f848ed0626cb3
Largo47 <ryu46@o2.pl>
2020-07-11 10:01:04 +0200

    Messy solutions to no gravity and blocking on possession

--- Ghost.gd
+++ Ghost.gd
@@ -4,8 +4,8 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-export var possessed = false
+var counter : int = 0
+var possessed : int = 1
 export var speed : int 
 export var JumpForce : int 
 export var gravity : int 
@@ -29,13 +29,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if possessed: 
-		vel.x = 0
-		vel.y += gravity
+	vel.x = 0
+	vel.y = gravity
+	if possessed == 1: 
+		print(possessed)
+		print(counter)
 		if Input.is_action_pressed("ui_left"):
 			vel.x -= speed
 		if Input.is_action_pressed("ui_right"):
 			vel.x += speed
-		vel = move_and_slide(vel,Vector2.UP)
-	else:
-		print("Doing my thing")
+		if Input.is_action_pressed("ui_accept"):  ### this is a mess
+			if possessed == 1:
+				possessed = 2  ### mess ends
+	elif possessed == 2: 
+		 ### also mess
+		if possessed == 2:
+			possessed = 1  ### mess ends
+		print(possessed)
+		print(counter)
+	vel = move_and_slide(vel,Vector2.UP)
+	counter += 1
