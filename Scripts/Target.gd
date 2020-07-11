37ef4f155cf7d3927f60671bf35d3fe6e07d828f
JackFly26 <jackfleishman@outlook.com>
2020-07-11 11:00:07 -0400

    better controls

--- Scripts/Target.gd
+++ Scripts/Target.gd
@@ -4,14 +4,16 @@ signal my_signal(target, collision, tileNumber);
 
 onready var sprite : Sprite = $Sprite
 
-export var speed : int 
-export var JumpForce : int 
-export var gravity : int 
-export var tileNumber : int
+export var acceleration = 512
+export var max_speed = 64
+export var friction = .02
+export var ground_friction = .1
+export var gravity = 400
+export var extraGravity = 200
+export var jump_power = -225
 
-var vel : Vector2 = Vector2()
+var velocity : Vector2 = Vector2()
 var possessed : bool
-var jumping : bool
 
 func possess():
 	print(get_name() + " - target possess");
@@ -28,33 +30,25 @@ func _ready():
 	print("target is ready");
 	
 func handle_input(delta):
-	if possessed:
-		if Input.is_key_pressed(KEY_A):
-			jumping = true;
-		else: 
-			jumping = false;
-		if Input.is_action_pressed("ui_left"):
-			vel.x -= speed
-		if Input.is_action_pressed("ui_right"):
-			vel.x += speed
-
-func _physics_process(delta):
-	vel.x = 0;
-	
-	handle_input(delta);
-	
-	if (is_on_floor()):
-		if (jumping):
-			vel.y = -JumpForce;
-		else:
-			vel.y = 0;
+	var input = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
+	if input != 0:
+		velocity.x += clamp(input * acceleration * delta, -max_speed, max_speed)
+	elif is_on_floor():
+		velocity.x = lerp(velocity.x, 0, ground_friction)
 	else:
-		vel.y += delta * gravity;
+		velocity.x = lerp(velocity.x, 0, friction)
+	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or is_on_wall()):
+		velocity.y = jump_power
+	elif velocity.y > 0:
+		velocity.y += (gravity + extraGravity) * delta
+	else:
+		velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func _physics_process(delta):	
+	if possessed: handle_input(delta);
 	
-	vel.x *= delta;
-	vel = move_and_slide(vel, Vector2.UP)
-	
-	for i in get_slide_count():
-		var collision = get_slide_collision(i);
-		if collision && collision.collider is TileMap && possessed:
-			emit_signal("my_signal", self, collision, tileNumber);
+#	for i in get_slide_count():
+#		var collision = get_slide_collision(i);
+#		if collision && collision.collider is TileMap && possessed:
+#			emit_signal("my_signal", self, collision, tileNumber);
