2718fa3ede2fe11fce252aad9200c9c80ebcfddb
11Lime <53125109+11lime@users.noreply.github.com>
2020-07-12 12:19:10 +0200

    Increase camera limit on the top a little

--- Scripts/LevelLogic.gd
+++ Scripts/LevelLogic.gd
@@ -12,13 +12,16 @@ func restrictCamera():
 	var tileMapDimension : Rect2 = tileMap.get_used_rect();
 	var tileSize = tileMap.cell_size;
 	
-	var realTopLeft = Vector2(tileSize * tileMapDimension.position);
-	var realMapDimension = Rect2(realTopLeft, tileSize * tileMapDimension.size);
+	# give some space to the top to prevent claustrophic feeling	
+	var offsetPosition = tileMapDimension.position - Vector2(0, 3);
 	
-	camera.limit_left = realMapDimension.position.x;
-	camera.limit_top = realMapDimension.position.y;
-	camera.limit_right = realMapDimension.end.x;
-	camera.limit_bottom = realMapDimension.end.y;
+	var topLeftWithOffset = Vector2(tileSize * offsetPosition);
+	var mapDimension = Rect2(topLeftWithOffset, tileSize * tileMapDimension.size);
 	
-	parallaxBackground.scroll_limit_begin = realMapDimension.position;
-	parallaxBackground.scroll_limit_end = realMapDimension.end;
+	camera.limit_left = mapDimension.position.x;
+	camera.limit_top = mapDimension.position.y;
+	camera.limit_right = mapDimension.end.x;
+	camera.limit_bottom = mapDimension.end.y;
+	
+	parallaxBackground.scroll_limit_begin = mapDimension.position;
+	parallaxBackground.scroll_limit_end = mapDimension.end;
